7bb12ff18947ccae8811f0c5ed4d902d9ce3a0b1
Kaviarasan-R <kaviarasanofficial26@gmail.com>
2024-11-26 20:33:01 +0530

    removed dist

--- dist/server/apps/server/src/app.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { UtilsService } from '@turborepo/utils';
-export declare class AppService {
-    private readonly configService;
-    private readonly utilsService;
-    constructor(configService: ConfigService, utilsService: UtilsService);
-    getHello(): string;
-}
